c0b2ac497461ac472dddacaa4370c4c505ded77e
hudsonjhonson9-arch <hudsonjhonson9@gmail.com>
2026-09-05 09:55:40 +0800

    fix: remove unused useCallback import

--- src/pages/PetaSebaranPage.tsx
+++ src/pages/PetaSebaranPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, GeoJSON, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
